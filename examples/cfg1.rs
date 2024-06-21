b7d8aa30539b641c69f6469e26ef280e264d1c43
Haruo <haruo-mtok@outlook.com>
2024-06-21 20:05:38 +0900

    [Add] examples

---
+++ examples/cfg1.rs
@@ -0,0 +1,20 @@
+// Context free grammer
+// S -> aSb | ε
+
+use monapa::*;
+
+fn start() -> Parser<()> {
+    (pdo! {
+        single('a');
+        start();
+        single('b');
+        return ()
+    }) | Parser::ret(())
+}
+
+fn main() {
+    let parser = start();
+
+    assert!(parser.clone().parse("aabb").is_ok());
+    assert!(parser.parse("aabb").is_ok());
+}
